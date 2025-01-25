dddc59da0100c0409af9b040f38127b6ba96a48f
Ruizhou <vm23152@bristol.ac.uk>
2025-01-26 01:13:19 +0800

    new file:   article/__init__.py 	new file:   article/admin.py 	new file:   article/apps.py 	new file:   article/migrations/0001_initial.py 	new file:   article/migrations/__init__.py 	new file:   article/models.py 	new file:   article/tests.py 	new file:   article/urls.py 	new file:   article/views.py 	new file:   db.sqlite3 	new file:   manage.py 	new file:   my_blog/__init__.py 	new file:   my_blog/settings.py 	new file:   my_blog/urls.py 	new file:   my_blog/wsgi.py 	new file:   static/bootstrap/css/.DS_Store 	new file:   static/bootstrap/css/bootstrap-grid.css 	new file:   static/bootstrap/css/bootstrap-grid.css.map 	new file:   static/bootstrap/css/bootstrap-grid.min.css 	new file:   static/bootstrap/css/bootstrap-grid.min.css.map 	new file:   static/bootstrap/css/bootstrap-reboot.css 	new file:   static/bootstrap/css/bootstrap-reboot.css.map 	new file:   static/bootstrap/css/bootstrap-reboot.min.css 	new file:   static/bootstrap/css/bootstrap-reboot.min.css.map 	new file:   static/bootstrap/css/bootstrap.css 	new file:   static/bootstrap/css/bootstrap.css.map 	new file:   static/bootstrap/css/bootstrap.min.css 	new file:   static/bootstrap/css/bootstrap.min.css.map 	new file:   static/bootstrap/js/bootstrap.bundle.js 	new file:   static/bootstrap/js/bootstrap.bundle.js.map 	new file:   static/bootstrap/js/bootstrap.bundle.min.js 	new file:   static/bootstrap/js/bootstrap.bundle.min.js.map 	new file:   static/bootstrap/js/bootstrap.js 	new file:   static/bootstrap/js/bootstrap.js.map 	new file:   static/bootstrap/js/bootstrap.min.js 	new file:   static/bootstrap/js/bootstrap.min.js.map 	new file:   static/jquery/jquery-3.7.1.js 	new file:   templates/article/detail.html 	new file:   templates/article/list.html 	new file:   templates/base.html 	new file:   templates/footer.html 	new file:   templates/header.html

---
+++ article/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = "article"
+
+urlpatterns = [
+    path('article-list/', views.article_list, name='article_list'),
+    # 注意<int:id>之间不能有空格，如果后续还有传入参数，在<int:id>/后面继续写
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    
+]
